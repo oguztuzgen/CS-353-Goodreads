92ab182d5a88db7449de96935237ff5e0911cea1
Oğuz Tüzgen <oguztuzgen@gmail.com>
2021-05-12 01:44:58 +0300

    finished uploading book cover started profile page and book page

---
+++ src/book_page.php
@@ -0,0 +1,19 @@
+<?php
+    require("template/header.php");
+    require('template/config.php');
+?>
+
+
+<br><br><br><br><br> <br>
+
+<div class="row">
+
+</div>
+
+<?php
+    echo $_SESSION['xd'];
+?>
+
+</body>
+</html>
+
