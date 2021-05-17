860894e48c8ab312f58a165243cb0056fc1a1773
Oğuz Tüzgen <oguztuzgen@gmail.com>
2021-05-17 05:57:05 +0300

    many changes
    Co-authored-by: muzafferkoksal <muzafferkoksal@users.noreply.github.com>
    Co-authored-by: Yiğit Harun <yigit2403@users.noreply.github.com>
    Co-authored-by: Uğur Utku Seyfeli <utkuseyfeli@users.noreply.github.com>

---
+++ src/club_page.php
@@ -0,0 +1,101 @@
+<?php
+	// error_reporting(0);
+	require('template/config.php');
+	require('template/header.php');
+
+	// HANDLE POST REQUESTS HERE
+
+
+	// UNTIL HERE
+	if (!isset($_GET['club_id'])) {
+		echo "<br><br><br><br><br><h1>404 NOT FOUND</h1>";
+    die;
+	}
+
+	$club_id = $_GET['club_id'];   
+
+    
+?>
+
+
+<!DOCTYPE html>
+
+<html>
+	<br><br><br><br><br><br>
+
+	<div class="row">
+		<div class="col s8">
+            <?php
+                $sql = "SELECT * FROM bc_post WHERE post_id IN (SELECT post_id FROM bc_post_belongs WHERE bc_id = $club_id) " ;
+                if ($result = mysqli_query($conn, $sql)) {
+                    $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                }
+                else{
+                        echo "FAILED TO EXECUTE QUERY";
+                }
+                $sql1 = "SELECT * FROM book_club WHERE club_id = '$club_id'";
+                if ($res1 = mysqli_query($conn, $sql1)) {
+                    $result1 = mysqli_fetch_array($res1, MYSQLI_ASSOC);
+                    echo " <h1>" .$result1["name"]. "</h1>";
+                    $sql2 = "SELECT book_cover FROM book WHERE book_id =(SELECT book_id FROM book_club WHERE club_id = $club_id)";
+                    $res2 = mysqli_query($conn, $sql2);
+                    $result2 = mysqli_fetch_array($res2, MYSQLI_ASSOC);
+                    echo "<img src=\"" . $result2["book_cover"] . "\"  width=\"300\" height=\"405\"  alt= \"No image\"> ";
+                    echo " <h4>" . "Member count = " . $result1["member_count"]. "</h4>";
+
+                }
+                else{
+                        echo "FAILED TO EXECUTE QUERY";
+                }
+                
+            ?> 
+            <?php foreach($result as $res): ?>
+                <?php
+                $pid = $res["post_id"];
+                //echo $pid;
+                $sql3 = "SELECT * FROM user WHERE user_id IN(SELECT user_id FROM posts WHERE post_id = $pid) ";
+                $res3 = mysqli_query($conn, $sql3);
+                $result3 = mysqli_fetch_array($res3, MYSQLI_ASSOC);
+                ?>
+                <table style = "width:100%">
+                    <tr>
+                        <td style = "text-align:left"> <?php echo "<h4>" .$result3["first_name"]. "</h4>" ?></td>
+                        <td style = "text-align:left"> <?php echo $res["content"];?> </td>		 
+                    </tr>
+                </table>
+
+                
+                
+            <?php endforeach ?>
+
+            <input type="submit" name="comment" class="btn blue lighten-1" value="Submit Review" style="margin:auto">
+            <?php if (isset($_POST['baban'])) {
+
+                    $sql = "insert into review (content, rating) values ('" . $_POST['reviewBox'] . "', " . $_COOKIE["rating"] . ");";
+                    if (mysqli_query($conn, $sql)) {
+                        //echo 'za';
+                    }
+
+                    $sql = "select max(review_id)
+                    from review;";
+                    $res = mysqli_query($conn, $sql);
+                    $rev_id = mysqli_fetch_array($res, MYSQLI_ASSOC);
+
+                    $sql = "insert into reviews(review_id, user_id, book_id) values (" . $rev_id['max(review_id)'] . ", " . $_SESSION['user_id'] . "," . $book_id . ");";
+                    if (mysqli_query($conn, $sql)) {
+                        //echo 'succesful insertion';
+                    }
+                }
+            ?>
+
+
+		</div>
+		<div class="col s4">
+			
+		</div>
+	</div>
+	
+
+</body>
+
+</html>
--- src/profile.php
+++ src/profile.php
@@ -2,15 +2,24 @@
 require('template/header.php');
 if (!$conn) {
 	echo "Error in connection";
+	die;
 }
+
 
 $uid = $_GET['uid'];
 $sql = 'SELECT * FROM user WHERE user_id = \'' . $uid . '\'';
+
+if (isset($_POST['edit_profile'])) {
+	// send somewhere else
+	header("Location: edit_profile.php?user_id=$uid");
+}
 
 echo $sql;
 if ($res = mysqli_query($conn, $sql)) {
 	$info = mysqli_fetch_array($res, MYSQLI_ASSOC);
 
+	$critic = $info['critic'];
+	$author = $info['author'];
 	//mysqli_free_result($res);
 	$id = $info['user_id'][0];
 	$sql1 = 'select * 
@@ -26,7 +35,20 @@ if ($res = mysqli_query($conn, $sql)) {
 }
 
 ?>
+<?php
+if (isset($_POST['remove'])) {
+	//echo $_POST['fr_id'];
 
+	$sql = "delete from friends where friend_id = " . $_POST['fr_id'] . " and user_id = " . $uid . " ;";
+	mysqli_query($conn, $sql);
+
+	/* delete from the other user too */
+	$sql = "delete from friends where friend_id = " . $uid . " and user_id = " . $_POST['fr_id'] . " ;";
+	mysqli_query($conn, $sql);
+
+	header("Location: profile.php?uid=$uid");
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -36,30 +58,69 @@ if ($res = mysqli_query($conn, $sql)) {
 
 
 <div class="row white text" style="width: 60%; float:left; margin-left:30px; padding: 20px;">
+	<?php echo "<form action=\"profile.php?uid=$uid\" method=\"post\">"; ?>
+
 
 	<div class="col">
-		<?php $prof = $info['profile_picture'];
-		echo "<img src=\"$prof\" width=\"250\" height=\"250\" alt='Bune'>"; ?>
-
-		<h5 style="text-decoration:underline; margin-left: 25px;"> Biography:</h5>
-		<div class="" style="margin-left: 25px; border: 3px solid grey; width: 100%; height: 100px;">
-			<?php echo '<h6>' . $info['about'] . '</h6>'; ?>
+		<div class="col">
+			<?php $prof = $info['profile_picture'];
+			echo "<img src=\"$prof\" width=\"250\" height=\"250\" alt='Bune'>"; ?>
 		</div>
+
+		<div class="col" style="margin-left: 12px;">
+			<h5 style="text-decoration:underline; margin-left: 25px;"> Biography:</h5>
+			<div class="" style="margin-left: 25px; border: 3px solid grey; width: 100%; height: 100px;">
+				<?php echo '<h6>' . $info['about'] . '</h6>'; ?>
+			</div>
+		</div>
+
 
 	</div>
 
+	<div class="row">
+
+	</div>
 	<div class="col">
 		<?php
 		echo '<h3>';
 		echo $info['first_name'] . " " . $info['last_name'];
 		echo '</h3>';
+		if ($critic) {
+			echo '<h3>';
+			echo '<span style="color:red; text-decoration:underline;" > *Critic* </span>';
+			echo '</h3>';
+		}
+
+		if ($author) {
+			echo '<h3>';
+			echo '<span style="color:blue; text-decoration:underline;" > *Author* </span>';
+			echo '</h3>';
+		}
 
 		echo '<h5>';
 		echo $info['karma'] . " karma";
 		echo '</h5>';
 		?>
-	</div>
 
+		<?php
+
+		if (!isset($_SESSION['user_id'])) {
+			// DO NOTHING
+		} else if ($_SESSION['user_id'] == $uid) { // if logged in user
+			echo "
+				<div class=\"col\">
+					<input type=\"submit\" name=\"edit_profile\" class=\"btn brand-btn\" value=\"Edit Profile\">
+				</div>
+			";
+		} else if (isset($_SESSION['user_id'])) { // if not the logged in user
+			echo "
+				<div class=\"col\">
+					<input type=\"submit\" name=\"friend\" class=\"btn brand-btn\" value=\"Add Friend\">
+				</div>
+			";
+		}
+		?>
+	</div>
 </div>
 
 <div class="row white text" style="width: 35%; float:right; margin-right:30px; padding: 20px;">
@@ -79,11 +140,21 @@ if ($res = mysqli_query($conn, $sql)) {
 		echo "<img src=" . $friend['profile_picture'] . " width=\"125\" height=\"125\" onerror=this.src='../image/profile_placeholder.jpg'>";
 		echo '</div>';
 		echo '<h6>' . $friend['first_name'] . " " . $friend['last_name'] . "</h6>";
-		
+	?>
+		<form action="" method="POST">
+			<input type="hidden" value="<?php echo $friend['friend_id']; ?>" name="fr_id">
+
+			<input type="submit" class="btn" value="Remove Friend" name="remove">
+		</form>
+	<?php
 		echo '</div>';
 	}
 	?>
+
+
 </div>
+
+
 
 <div class="row" style="float: left; width: 60%; margin-left:30px;">
 	<hr>
@@ -98,18 +169,14 @@ if ($res = mysqli_query($conn, $sql)) {
 	$qry = 'select l.count, b.title, l.date
 	from logs l, book b
 	where l.user_id = ' . $uid . ' and l.book_id = b.book_id
-	order by l.date
-	limit 3';
+	order by l.date DESC 
+	limit 5';
 
 	$result = mysqli_query($conn, $qry);
 
 
 	$i = 0;
 	while ($log = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-
-		if ($i >= 3) {
-			break;
-		}
 
 		echo '<h6 style="margin-left:25px; font-size:18px"> <span style="color: red"> ' . $info['first_name'] . " " . $info['last_name'] . '</span> has read <span style="color: blue">' . $log['count'] . '</span> pages from the book ';
 
@@ -127,6 +194,32 @@ if ($res = mysqli_query($conn, $sql)) {
 
 </div>
 
+<div class="row white text" style="width: 35%; float:right; margin-right:30px; padding: 20px;">
+	<h5 style="text-decoration:underline; margin-left: 25px;"> Reading Challenge Wins:
+		<?php
+		$quer = "select wins from user where user_id = $uid ;";
+		$resultt = mysqli_query($conn, $quer);
+
+		$win = mysqli_fetch_array($resultt);
+		?>
+		<span style="color:red;"><?php echo $win['wins']; ?> </span>
+	</h5>
+
+	<h5 style="text-decoration:underline; margin-left: 25px;">Participated Reading Challenges:</h5>
+
+	<?php
+	$quer = "select * from rc_participates rc, reading_challenge rcs where rc.user_id = $uid and rcs.rc_id = rc.rc_id;";
+	$resultt = mysqli_query($conn, $quer);
+
+	while ($rc = mysqli_fetch_array($resultt)) {
+	?>
+
+		<h5 style="margin-left: 25px;"><?php echo $rc['subject'];  ?></h5>
+
+	<?php } ?>
+
+</div>
+
 <div class="row" style="float: left; width: 60%; margin-left:30px;">
 	<hr>
 </div>
@@ -135,7 +228,29 @@ if ($res = mysqli_query($conn, $sql)) {
 	<h5 style="text-decoration:underline; margin-left: 25px;"> Book Clubs:</h5>
 </div>
 
+<div class="row white text" style="width: 35%; float:right; margin-right:30px; padding: 20px;">
+	<h5 style="text-decoration:underline; margin-left: 25px;"> Recommend by Friends</h5>
+	
+	<?php 
+		$sql = "select *
+		from (select distinct book_id from recommends where friend_id = $uid) as c, book b
+		where c.book_id = b.book_id ;";
 
+		$res = mysqli_query($conn, $sql);
+
+		while ($bok = mysqli_fetch_array($res)) {
+			?>
+		
+			<img src="<?php echo $bok['book_cover'] ?>" alt="">
+
+
+	<?php			
+		}
+	?>
+
+</div>
+
+</form>
 
 </body>
 
